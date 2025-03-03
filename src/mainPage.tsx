b23910cd6f33ea216dc648ccecf95c049a84408a
Márk Mados <mark.mados@rapidimages.se>
2025-03-03 15:35:18 +0100

    new smaller videos

--- src/mainPage.tsx
+++ src/mainPage.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import "./mainPage.css";
 import gsap from "gsap";
 import { useMediaQuery } from "./contexts/MediaQueryContext";
